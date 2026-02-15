cecdd29d380cf94e705162242e384f02d2c887f7
Aleksey <aleksejj.kostromin@vitech.team>
2026-02-15 20:12:13 +0400

    HM_2

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 from framework.helpers.kafka.consumers.register_events import RegisterEventsSubscriber
+from framework.helpers.kafka.consumers.register_events_errors import RegisterEventsErrorsSubscriber
 from framework.internal.http.account import AccountApi
 from framework.internal.http.mail import MailApi
 from framework.internal.kafka.consumer import Consumer
@@ -28,7 +29,12 @@ def register_events_subscriber() -> RegisterEventsSubscriber:
     return RegisterEventsSubscriber()
 
 
+@pytest.fixture(scope="session")
+def register_events_errors_subscriber() -> RegisterEventsErrorsSubscriber:
+    return RegisterEventsErrorsSubscriber()
+
+
 @pytest.fixture(scope="session", autouse=True)
-def kafka_consumer(register_events_subscriber: RegisterEventsSubscriber) -> Consumer:
-    with Consumer(subscribers=[register_events_subscriber]) as consumer:
+def kafka_consumer(register_events_subscriber: RegisterEventsSubscriber, register_events_errors_subscriber: RegisterEventsErrorsSubscriber) -> Consumer:
+    with Consumer(subscribers=[register_events_subscriber,register_events_errors_subscriber]) as consumer:
         yield consumer
